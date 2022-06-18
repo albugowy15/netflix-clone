078fb8612717b7d1f7e7f8ac18d70d0d8a51b84f
albugowy15 <muhammadbughowy@gmail.com>
2022-06-18 21:38:53 +0700

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Main from "../components/Main";
 import Row from "../components/Row";
-import requests from "../Request";
+import requests from "../Requests";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Main />
-      <Row title="Up Coming" fetchUrl={requests.requestUpcoming} />
-      <Row title="Popular" fetchUrl={requests.requestPopular} />
-      <Row title="Trending" fetchUrl={requests.requestTrending} />
-      <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
-      <Row title="Horror" fetchUrl={requests.requestHorror} />
-    </div>
+      <Row rowID="1" title="UpComing" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="5" title="Horror" fetchURL={requests.requestHorror} />
+    </>
   );
 };
 
